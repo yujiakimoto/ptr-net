ad48de929337d6fa12b523ffd8615f9a2c260a6d
yujiakimoto <ya242@cornell.edu>
2018-03-27 03:16:23 -0400

    Debugging

--- model.py
+++ model.py
@@ -13,25 +13,23 @@ UNITS = 25
 LAYERS = 2
 
 
-
 class PointerNet(object):
     def __init__(self):
-        word_matrix = np.random.random([200,200])
+        word_matrix = np.random.random([40000, 50])
         # input placeholder for word sequence
         self.encode_input = tf.placeholder(tf.int32, [BATCH_SIZE, MAX_LENGTH], name='encoded_input')
         self.seq_lengths = tf.placeholder(tf.int32, [BATCH_SIZE], name='sequence_lengths')
 
         # encode the input sequence to glove
         self.word_matrix = tf.Variable(tf.constant(word_matrix), trainable=False, name='word_matrix')
-        self.embeded_input = tf.nn.embedding_lookup(self.word_matrix, self.encode_input, name='embeded_input')
+        self.embedded_input = tf.nn.embedding_lookup(self.word_matrix, self.encode_input, name='embedded_input')
 
         # RNN embedding of the input sequence
-        cell = CELL(UNITS)
-        init_state = (tf.zeros([BATCH_SIZE, UNITS]), tf.zeros([BATCH_SIZE, UNITS]))
-        cell_fw = tf.contrib.rnn.MultiRNNCell([cell for _ in range(LAYERS)])
+        cell_fw = tf.contrib.rnn.MultiRNNCell([CELL(UNITS) for _ in range(LAYERS)])
+        init_state = cell_fw.zero_state(BATCH_SIZE, dtype=tf.float64)
 
-        self.outputs, state = tf.nn.dynamic_rnn(cell_fw, self.embeded_input, sequence_length=self.seq_lengths, initial_state=init_state,
-                                                dtype=tf.float32)
+        self.outputs, state = tf.nn.dynamic_rnn(cell_fw, self.embedded_input, sequence_length=self.seq_lengths,
+                                                initial_state=init_state, dtype=tf.float64)
 
 
 if __name__ == '__main__':
