82752d3f1d7f52c73fc01e9885c7375939ba81a2
yujiakimoto <ya242@cornell.edu>
2018-03-29 21:42:49 -0400

    Updated training script

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ def train(args):
     validation = Loader(os.path.join(args.data_dir, 'validate.txt'), vocab_path, args.batch_size, 45)
 
     # create TensorFlow graph
-    ptr_net = PointerNet()
+    ptr_net = PointerNet(batch_size=args.batch_size, learning_rate=args.learning_rate)
     saver = tf.train.Saver()
     best_val_acc = 0
 
@@ -34,7 +34,9 @@ def train(args):
             tr_loss, tr_acc = 0, 0
             for itr in range(training.n_batches):
                 x_batch, x_lengths, y_batch = training.next_batch()
-                train_dict = {ptr_net.inputs: x_batch, ptr_net.input_lengths: x_lengths, ptr_net.outputs: y_batch}
+                train_dict = {ptr_net.encoder_inputs: x_batch,
+                              ptr_net.input_lengths: x_lengths,
+                              ptr_net.pointer_labels: y_batch}
                 loss, acc, _ = sess.run([ptr_net.loss, ptr_net.exact_match, ptr_net.train_step], feed_dict=train_dict)
                 tr_loss += loss
                 tr_acc += acc
@@ -42,20 +44,22 @@ def train(args):
             train_losses.append(tr_loss / training.n_batches)
             train_accuracies.append(tr_acc / training.n_batches)
 
-            # check validation accuracy every 100 epochs
-            if ep % 100 == 0:
+            # check validation accuracy every 10 epochs
+            if ep % 10 == 0:
                 val_acc = 0
                 for itr in range(validation.n_batches):
                     x_batch, x_lengths, y_batch = validation.next_batch()
-                    val_dict = {ptr_net.inputs: x_batch, ptr_net.input_lengths: x_lengths, ptr_net.outputs: y_batch}
+                    val_dict = {ptr_net.encoder_inputs: x_batch,
+                                ptr_net.input_lengths: x_lengths,
+                                ptr_net.pointer_labels: y_batch}
                     val_acc += sess.run(ptr_net.exact_match, feed_dict=val_dict)
                 val_acc = val_acc / validation.n_batches
 
-                print("epoch {:3d}, loss={:.2f}".format(ep, tr_loss / training.n_batches))
-                print("Train EM: {:.2f}, Validation EM: {:.2f}".format(tr_acc / training.n_batches, val_acc))
+                print('epoch {:3d}, loss={:.2f}'.format(ep, tr_loss / training.n_batches))
+                print('Train EM: {:.2f}, Validation EM: {:.2f}'.format(tr_acc / training.n_batches, val_acc))
 
                 # save model
-                if val_acc > best_val_acc:
+                if val_acc >= best_val_acc:
                     print('Validation accuracy increased. Saving model.')
                     saver.save(sess, os.path.join(args.save_dir, 'ptr_net.ckpt'))
                     best_val_acc = val_acc
@@ -71,8 +75,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='./data', help='Directory in which data is stored.')
     parser.add_argument('--save_dir', type=str, default='./models', help='Where to save checkpoint models.')
-    parser.add_argument('--n_epochs', type=int, default=10000, help='Number of epochs to run.')
+    parser.add_argument('--n_epochs', type=int, default=100, help='Number of epochs to run.')
     parser.add_argument('--batch_size', type=int, default=100, help='Batch size.')
-    parser.add_argument('--learning_rate', type=float, default=0.01, help='Learning rate for Adam optimizer.')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate for Adam optimizer.')
     args = parser.parse_args(sys.argv[1:])
     train(args)
